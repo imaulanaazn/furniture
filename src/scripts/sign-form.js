ff8f4eb745dfb0de18779e3b3b7509dd756894d4
Irham Maulana <i.maulana.azn@gmail.com>
2022-06-01 11:19:44 +0700

    menyelesaikan sign in dan sign up page

---
+++ src/scripts/sign-form.js
@@ -0,0 +1,10 @@
+const asideBtn = document.querySelector('aside button');
+const main = document.querySelector('main');
+let isSignUp = true;
+
+asideBtn.addEventListener('click',()=>{
+    if(isSignUp){
+        
+    }
+})
+
